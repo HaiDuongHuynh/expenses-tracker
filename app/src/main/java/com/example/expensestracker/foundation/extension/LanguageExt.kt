2a017a92f56adf5d29d8d2bf02d682af1c8e77eb
Hai Duong Huynh <106815704+HaiDuongHuynh@users.noreply.github.com>
2025-06-17 13:14:38 -0500

    a

--- app/src/main/java/com/example/expensestracker/foundation/extension/LanguageExt.kt
+++ app/src/main/java/com/example/expensestracker/foundation/extension/LanguageExt.kt
@@ -5,6 +5,7 @@ import com.example.expensestracker.foundation.datasource.preference.model.Langua
 fun LanguagePreference.toLanguage(): com.example.expensestracker.model.Language {
     return when (this.code) {
         com.example.expensestracker.model.Language.INDONESIA.code -> com.example.expensestracker.model.Language.INDONESIA
+        com.example.expensestracker.model.Language.VIETNAM.code -> com.example.expensestracker.model.Language.VIETNAM
         else -> com.example.expensestracker.model.Language.ENGLISH
     }
 }
